706bfc3fd4387739cd378207bc70964b4159166a
zjburas <zjburas@mit.edu>
2017-03-02 20:28:51 -0500

    Created tool to fit a fourier series (or cosine) to Hindered rotor scan data (in .txt format).
    Uses Cantherm's function to perform fit, and decide which is best.
    Outputs fit parameters and plot.
    
    The data being fit need not be Potential Energy, it could be any
    periodic data (such as moment of inertia of the rotor, which can also
    vary with dihedral angle and must be fit to a Fourier Series for using
    the software Multiwell).

---
+++ Cantherm_input/Fit_Fourier.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+
+Run using the  following commands:
+
+python Evaluate_NASA_polynomials.py "path_to_chemkin_file"
+"""
+
+import argparse
+import os
+import rmgpy.cantherm.statmech
+from rmgpy.cantherm.statmech import HinderedRotor, ScanLog
+from rmgpy import settings
+from rmgpy.cantherm.output import prettify
+import numpy
+import rmgpy.constants as constants
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('scanLogPath', metavar='CHEMKIN', type=str, nargs=1,
+        help='The path of scanlog text file')
+
+    parser.add_argument('symmetry',type=int, nargs=1,
+        help='Symmetry of rotor scan')
+
+    args = parser.parse_args()
+    scanLogPath = args.scanLogPath[0]
+    symmetry = args.symmetry[0]
+
+    directory = os.path.abspath(os.path.dirname(scanLogPath))
+
+    #Save output in same directory as input
+    outputDirectory = os.path.dirname(os.path.abspath(scanLogPath))
+
+    # Initialize (and clear!) the output files for the job
+    # outputFile = os.path.join(outputDirectory, 'evaluated_NASA_polynomial.py')
+    # with open(outputFile, 'w') as f:
+    #     pass
+
+    #Load the scanlog file
+    scanlog=ScanLog(scanLogPath)
+    angle, Vlist = scanlog.load()
+
+    #Initialize HinderedRotor class
+    fourierRotor = HinderedRotor(inertia=(1, "amu*angstrom^2"), symmetry=symmetry)
+    fourierRotor.fitFourierPotentialToData(angle, Vlist)
+
+    #Fit scan to both cosine and fourier series
+    cosineRotor = HinderedRotor(inertia=(1, "amu*angstrom^2"), symmetry=symmetry)
+    cosineRotor.fitCosinePotentialToData(angle, Vlist)
+    fourierRotor = HinderedRotor(inertia=(1, "amu*angstrom^2"), symmetry=symmetry)
+    fourierRotor.fitFourierPotentialToData(angle, Vlist)
+
+    #Evaluate fits
+    Vlist_cosine = numpy.zeros_like(angle)
+    Vlist_fourier = numpy.zeros_like(angle)
+    for i in range(angle.shape[0]):
+        Vlist_cosine[i] = cosineRotor.getPotential(angle[i])
+        Vlist_fourier[i] = fourierRotor.getPotential(angle[i])
+
+    #Determine which fit is better
+    rms_cosine = numpy.sqrt(numpy.sum((Vlist_cosine - Vlist) * (Vlist_cosine - Vlist)) / (len(Vlist) - 1)) / 4184.
+    rms_fourier = numpy.sqrt(numpy.sum((Vlist_fourier - Vlist) * (Vlist_fourier - Vlist)) / (len(Vlist) - 1)) / 4184.
+
+    # Keep the rotor with the most accurate potential
+    rotor = cosineRotor if rms_cosine < rms_fourier else fourierRotor
+    # However, keep the cosine rotor if it is accurate enough, the
+    # fourier rotor is not significantly more accurate, and the cosine
+    # rotor has the correct symmetry
+    if rms_cosine < 0.05 and rms_cosine / rms_fourier < 2.0 and rms_cosine / rms_fourier < 4.0 and symmetry == cosineRotor.symmetry:
+        rotor = cosineRotor
+
+    """
+    Plot the potential for the rotor, along with its cosine and Fourier
+    series potential fits. The plot is saved to a set of files of the form
+    ``hindered_rotor_1.pdf``.
+    """
+
+    try:
+        import pylab
+    except ImportError:
+        error
+
+    fig = pylab.figure(figsize=(6, 5))
+    pylab.plot(angle, Vlist / 4184., 'ok')
+    linespec = '-r' if rotor is cosineRotor else '--r'
+    pylab.plot(angle, Vlist_cosine / 4184., linespec)
+    linespec = '-b' if rotor is fourierRotor else '--b'
+    pylab.plot(angle, Vlist_fourier / 4184., linespec)
+    pylab.legend(['scan', 'cosine', 'fourier'], loc=1)
+    pylab.xlim(0, 2 * constants.pi)
+    pylab.xlabel('Angle')
+    pylab.ylabel('Potential (kcal/mol)')
+
+    axes = fig.get_axes()[0]
+    axes.set_xticks([float(j * constants.pi / 4) for j in range(0, 9)])
+    axes.set_xticks([float(j * constants.pi / 8) for j in range(0, 17)], minor=True)
+    axes.set_xticklabels(
+        ['$0$', '$\pi/4$', '$\pi/2$', '$3\pi/4$', '$\pi$', '$5\pi/4$', '$3\pi/2$', '$7\pi/4$', '$2\pi$'])
+
+    pylab.savefig(os.path.join(directory, 'rotor_fit.pdf'))
+    pylab.close()
+
+    print(rotor)
